b21889927a8b8665b73d47aa43fb707978fbf543
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2024-11-21 23:49:44 +0100

    created a function playGame that includes playRound and repeats it 5x , made a for loop for it

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,8 +13,6 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
-
 // Criar opções de jogadas para o Jogador Real
 
 function getHumanChoice() {
@@ -24,40 +22,44 @@ function getHumanChoice() {
 
 // Criar marcação de Pontuação
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-// Criar Tabuleiro
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return "It's a tie!";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      humanScore++;
+      return "You win!";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      computerScore++;
+      return "You Loose!";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      computerScore++;
+      return "You Loose!";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      humanScore++;
+      return "You Win!";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      computerScore++;
+      return "You Loose!";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      humanScore++;
+      return "You Win!";
+    }
+  }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    return "It's a tie!";
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    humanScore++;
-    return "You win!";
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
-    return "You Loose!";
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore++;
-    return "You Loose!";
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore++;
-    return "You Win!";
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    computerScore++;
-    return "You Loose!";
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    computerScore++;
-    return "You Win!";
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    const result = playRound(humanSelection, computerSelection);
+    console.log(`You chose: ${humanSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log(result);
+    console.log(`Score - Human: ${humanScore}, Computer: ${computerScore}`);
   }
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-const result = playRound(humanSelection, computerSelection);
-console.log(`You chose: ${humanSelection}`);
-console.log(`Computer chose: ${computerSelection}`);
-console.log(result);
-console.log(`Score - Human: ${humanScore}, Computer: ${computerScore}`);
+playGame();
+// Criar 5 rondas
