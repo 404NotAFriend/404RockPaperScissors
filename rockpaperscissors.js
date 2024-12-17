63e98e99387618cc5e167ea244312b3601a01dc3
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2024-12-17 20:37:08 +0100

    Added formatting code to javascript file Created new css format for body etc Changed images for rock paper and scissors Created new css format for scoreBoard and buttons

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -69,27 +69,33 @@ rockButton.id = "rockButton";
 rockButton.innerHTML = '<img src="rock.png" />';
 rockButton.style.width = "300px";
 rockButton.style.height = "300px";
+rockButton.style.borderRadius = "10px";
 const img = rockButton.querySelector("img");
 img.style.width = "100%"; // Make the image fill the button's width
-img.style.height = "100%"; // Keep the image's aspect ratio
+img.style.height = "100%"; //
+img.style.borderRadius = "10px";
 
 const paperButton = document.createElement("button");
 paperButton.id = "paperButton";
 paperButton.innerHTML = '<img src="paper.png" />';
 paperButton.style.width = "300px";
 paperButton.style.height = "300px";
+paperButton.style.borderRadius = "10px";
 const img2 = paperButton.querySelector("img");
 img2.style.width = "100%"; // Make the image fill the button's width
 img2.style.height = "100%"; //
+img2.style.borderRadius = "10px";
 
 const scissorsButton = document.createElement("button");
 scissorsButton.id = "scissorsButton";
 scissorsButton.innerHTML = '<img src="scissors.png" />';
 scissorsButton.style.width = "300px";
 scissorsButton.style.height = "300px";
+scissorsButton.style.borderRadius = "10px";
 const img3 = scissorsButton.querySelector("img");
 img3.style.width = "100%"; // Make the image fill the button's width
 img3.style.height = "100%"; //
+img3.style.borderRadius = "10px";
 
 rockButton.addEventListener("click", () => {
   const humanChoice = "rock";
@@ -126,9 +132,31 @@ scissorsButton.addEventListener("click", () => {
 
 const board = document.createElement("div");
 board.style.display = "flex";
-board.style.justifyContent = "space-around";
+board.style.justifyContent = "space-between";
+board.style.gap = "20px";
+board.style.padding = "200px 0px";
 board.appendChild(rockButton);
 board.appendChild(paperButton);
 board.appendChild(scissorsButton);
 
 document.body.appendChild(board);
+
+const scoreBoard = document.createElement("div");
+scoreBoard.style.display = "flex";
+scoreBoard.style.justifyContent = "center";
+scoreBoard.style.border = "5px solid #F4F2F3";
+scoreBoard.style.borderRadius = "10px";
+scoreBoard.style.backgroundColor = "navy";
+scoreBoard.style.padding = "10px";
+
+const totalScore = document.createElement("h2");
+totalScore.textContent = `Total Score - Human: ${humanScore}, Computer: ${computerScore}`;
+totalScore.style.color = "white";
+scoreBoard.appendChild(totalScore);
+
+document.body.appendChild(scoreBoard);
+
+// Function to update the score board after each round
+function updateScoreBoard() {
+  totalScore.textContent = `Total Score - Human: ${humanScore}, Computer: ${computerScore}`;
+}
